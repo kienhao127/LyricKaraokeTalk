472daaaa7f08e8dfa75fea8e90417a1b23278c9e
kienhao127 <30612210+kienhao127@users.noreply.github.com>
2018-01-10 17:40:35 +0700

    create donut progress
    create donut progress

---
+++ app/src/main/java/com/example/cpu11341_local/lyrickaraoketalk/myview/DonutProgress.java
@@ -0,0 +1,178 @@
+package com.example.cpu11341_local.lyrickaraoketalk.myview;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.cpu11341_local.lyrickaraoketalk.R;
+
+/**
+ * Created by CPU11341-local on 10-Jan-18.
+ */
+
+public class DonutProgress extends View {
+    private Paint finishedPaint;
+    private Paint unfinishedPaint;
+
+    private int finishedStrokeColor;
+    private int unfinishedStrokeColor;
+    private float finishedStrokeWidth;
+    private float unfinishedStrokeWidth;
+
+    private RectF finishedOuterRect = new RectF();
+    private RectF unfinishedOuterRect = new RectF();
+
+    private final float default_stroke_width;
+    private final int default_finished_color = Color.rgb(66, 145, 241);
+    private final int default_unfinished_color = Color.rgb(204, 204, 204);
+
+    private float progress = 0;
+    private int max = 100;
+    private int startingDegree = 0;
+
+    public DonutProgress(Context context) {
+        this(context, null);
+    }
+
+    public DonutProgress(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DonutProgress(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        default_stroke_width = dp2px(getResources(), 5);
+
+        final TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DonutProgress, defStyleAttr, 0);
+        initByAttributes(attributes);
+        attributes.recycle();
+
+        initPainters();
+    }
+
+    private static float dp2px(Resources resources, float dp) {
+        final float scale = resources.getDisplayMetrics().density;
+        return  dp * scale + 0.5f;
+    }
+
+    protected void initPainters() {
+        finishedPaint = new Paint();
+        finishedPaint.setColor(finishedStrokeColor);
+        finishedPaint.setStyle(Paint.Style.STROKE);
+        finishedPaint.setAntiAlias(true);
+        finishedPaint.setStrokeWidth(finishedStrokeWidth);
+
+        unfinishedPaint = new Paint();
+        unfinishedPaint.setColor(unfinishedStrokeColor);
+        unfinishedPaint.setStyle(Paint.Style.STROKE);
+        unfinishedPaint.setAntiAlias(true);
+        unfinishedPaint.setStrokeWidth(unfinishedStrokeWidth);
+    }
+
+    protected void initByAttributes(TypedArray attributes) {
+        finishedStrokeColor = attributes.getColor(R.styleable.DonutProgress_donut_finished_color, default_finished_color);
+        unfinishedStrokeColor = attributes.getColor(R.styleable.DonutProgress_donut_unfinished_color, default_unfinished_color);
+
+        finishedStrokeWidth = attributes.getDimension(R.styleable.DonutProgress_donut_finished_stroke_width, default_stroke_width);
+        unfinishedStrokeWidth = attributes.getDimension(R.styleable.DonutProgress_donut_unfinished_stroke_width, default_stroke_width);
+
+        setProgress(attributes.getFloat(R.styleable.DonutProgress_donut_progress, 0));
+    }
+
+    public float getFinishedStrokeWidth() {
+        return finishedStrokeWidth;
+    }
+
+    public void setFinishedStrokeWidth(float finishedStrokeWidth) {
+        this.finishedStrokeWidth = finishedStrokeWidth;
+        this.invalidate();
+    }
+
+    public float getUnfinishedStrokeWidth() {
+        return unfinishedStrokeWidth;
+    }
+
+    public void setUnfinishedStrokeWidth(float unfinishedStrokeWidth) {
+        this.unfinishedStrokeWidth = unfinishedStrokeWidth;
+        this.invalidate();
+    }
+
+    private float getProgressAngle() {
+        return getProgress() / (float) max * 360f;
+    }
+
+    public float getProgress() {
+        return progress;
+    }
+
+    public void setProgress(float progress) {
+        this.progress = progress;
+        if (this.progress > getMax()) {
+            this.progress %= getMax();
+        }
+        invalidate();
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        if (max > 0) {
+            this.max = max;
+            invalidate();
+        }
+    }
+
+    public int getFinishedStrokeColor() {
+        return finishedStrokeColor;
+    }
+
+    public void setFinishedStrokeColor(int finishedStrokeColor) {
+        this.finishedStrokeColor = finishedStrokeColor;
+        this.invalidate();
+    }
+
+    public int getUnfinishedStrokeColor() {
+        return unfinishedStrokeColor;
+    }
+
+    public void setUnfinishedStrokeColor(int unfinishedStrokeColor) {
+        this.unfinishedStrokeColor = unfinishedStrokeColor;
+        this.invalidate();
+    }
+
+    public int getStartingDegree() {
+        return startingDegree;
+    }
+
+    public void setStartingDegree(int startingDegree) {
+        this.startingDegree = startingDegree;
+        this.invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float delta = Math.max(finishedStrokeWidth, unfinishedStrokeWidth);
+        finishedOuterRect.set(delta,
+                delta,
+                getWidth() - delta,
+                getHeight() - delta);
+        unfinishedOuterRect.set(delta,
+                delta,
+                getWidth() - delta,
+                getHeight() - delta);
+
+        canvas.drawArc(finishedOuterRect, getStartingDegree(), getProgressAngle(), false, finishedPaint);
+        canvas.drawArc(unfinishedOuterRect, getStartingDegree() + getProgressAngle(), 360 - getProgressAngle(), false, unfinishedPaint);
+    }
+}
